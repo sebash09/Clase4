9fadb5b8fbaa7802cf007db31177fa9dcee83071
sebash09 <66684387+sebash09@users.noreply.github.com>
2024-03-15 23:37:17 -0500

    Update accesoBD.php

--- accesoBD.php
+++ accesoBD.php
@@ -12,7 +12,7 @@ class accesoBD {
         $res = conexion::ejecutarSQL($host);
         if ($res->num_rows > 0) {
             while ($campos = $res->fetch_object()) {
-                $pArray->things[] = array('id' => $campos->id, 'valor' => $campos->value, 'fecha' => $campos->date, 'tipo' => $campos->type);
+                $pArray->things[] = array('id' => $campos->id, 'id_device' => $campos->id_device, 'valor' => $campos->value, 'fecha' => $campos->date, 'tipo' => $campos->type);
             }
             $pArray->code = '200';
         } else {
@@ -22,10 +22,10 @@ class accesoBD {
         return $pArray;
     }
 
-    public static function subirDatos($value, $type) {
+    public static function subirDatos($value, $type, $id_device) {
         $pArray = new stdClass();
         $fecha = date('Y-m-d H:i:s');
-        $sql = "INSERT INTO things (type, value, date) VALUES ('{$type}', '{$value}', '{$fecha}')";
+        $sql = "INSERT INTO things (type, value, date, id_device) VALUES ('{$type}', '{$value}', '{$fecha}', '{$id_device}')";
         if (conexion::ejecutarSQL($sql)) {
             $pArray->code = '200';
         } else {
@@ -36,5 +36,6 @@ class accesoBD {
     }
 }
 ?>
+
 
 
